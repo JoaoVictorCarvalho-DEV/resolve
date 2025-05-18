d54758b6b6000a502cfda3ed16b91d4df7842007
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-05-18 09:37:48 -0300

    first commit

---
+++ database/migrations/2025_05_17_205903_create_solution_tag_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solution_tag', function (Blueprint $table) {
+            $table->foreignId('solution_id')->constrained('solutions')->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade');
+            $table->timestamps();
+
+            $table->primary(['solution_id'],'tag_id');//evita que o mesmo relacionamento seja criado
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solution_tag');
+    }
+};
