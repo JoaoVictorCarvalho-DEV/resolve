18c1c074d294bb5fd1fc7ea0ac8abfd7f5faefdd
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-05-18 09:46:21 -0300

    Terminada a criação dos models

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    protected $fillable =  [
+        'user_id',
+        'solution_id',
+    ];
+
+    public function solution(){
+        return $this->belongsTo(Solution::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+
+    public function solution(){
+        return $this->belongsToMany(Solution::class);
+    }
 }
