5afe5aed5de8a513ec5a936de6ab6397ad9ee104
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-05-22 11:08:46 -0300

    feat: routes created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\SolutionController;
+use App\Http\Controllers\TagController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('solutions', SolutionController::class);
+
+Route::resource('comments', CommentController::class);
+
+Route::resource('tags', TagController::class);
+
+Route::resource('likes', LikeController::class);
+
