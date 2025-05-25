335dcf6b3515c7662edcf2c60bd276db626bc645
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-05-25 12:55:49 -0300

    style: template changed

--- resources/views/solution/create.blade.php
+++ resources/views/solution/create.blade.php
@@ -8,7 +8,7 @@
                 {{ session('success') }}
             </div>
         @endif
-        
+
         <form action="/solutions" method="POST" class="space-y-4">
             @csrf
 
@@ -21,8 +21,7 @@
             <div>
                 <label for="description" class="block text-sm font-medium text-gray-700">Descrição</label>
                 <textarea name="description" id="description" rows="4"
-                    class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
-                </textarea>
+                    class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"></textarea>
             </div>
 
             <div>
